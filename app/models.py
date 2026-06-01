c4c7658b6edade652713e6521695de1ccbf33ce0
burrnt <pogoriliy4@gmail.com>
2026-06-01 18:43:53 +0300

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+from sqlalchemy import (Column, Integer, String, Boolean,
+                     	Float, DateTime, ForeignKey, Table, Text)
+from sqlalchemy.orm import relationship
+from app.database import Base
