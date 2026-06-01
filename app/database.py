dc5a43f178a886f714310527410ef8352086c9a1
burrnt <pogoriliy4@gmail.com>
2026-06-01 16:19:03 +0300

    Update database.py
    Updated database connection settings and refactored ORM base class.

--- app/database.py
+++ app/database.py
@@ -1,12 +1,19 @@
-# Підключення до SQLite
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
-
-DATABASE_URL = "sqlite:///./test.db"
-
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+ 
+DATABASE_URL = "sqlite:///./data/app.db"
+ 
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+	DATABASE_URL,
+	connect_args={"check_same_thread": False},
 )
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
+ 
+SessionLocal = sessionmaker(
+	autocommit=False,
+	autoflush=False,
+	bind=engine,
+)
+ 
+ 
+class Base(DeclarativeBase):
+	pass
