51899f448dec081737f5e474b19a745147b61051
burrnt <pogoriliy4@gmail.com>
2026-06-06 16:38:10 +0300

    Import models

--- alembic/env.py
+++ alembic/env.py
@@ -20,6 +20,7 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 from app.database import Base
+from app import models
 target_metadata = Base.metadata
 
 
