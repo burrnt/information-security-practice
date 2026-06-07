758dcf17eb7a09188d214984ccf476df279071ef
burrnt <pogoriliy4@gmail.com>
2026-06-07 16:55:33 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
+from app.routers import auth
 from app.database import Base, engine
 from app import models
  
-app = FastAPI(title="Electronic Dean's Office")
- 
+app = FastAPI(
+    title="Електронний деканат",
+    description="API для управління академічними даними",
+    version="0.4.0"
+)
+
+# Підключення роутерів
+app.include_router(auth.router)
  
 @app.get("/")
 def root():
-	return {"message": "Electronic Dean's Office API"}
+	return {"message":"Електронний деканат API v0.4.0"}
  
  
 @app.get("/health")
