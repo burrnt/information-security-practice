c69c081d41111dfe28f61b030af640faf76838ec
burrnt <pogoriliy4@gmail.com>
2026-06-07 14:40:10 +0300

    Refactor sys.path insertion and update comments

--- alembic/env.py
+++ alembic/env.py
@@ -6,28 +6,28 @@ from alembic import context
 import os
 import sys
 
-# Add the app directory to the Python path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-# This is the Alembic Config object
+sys.path.insert(
+    0,
+    os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+)
+
 config = context.config
 
-# Interpret the config file for Python logging
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# Add your model's MetaData object for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
 from app.database import Base
 from app import models
+
 target_metadata = Base.metadata
 
 
-
 def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode."""
+    """Run migrations in offline mode."""
+
     url = config.get_main_option("sqlalchemy.url")
+
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -40,9 +40,12 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
-    """Run migrations in 'online' mode."""
-    configuration["sqlalchemy.url"] = config.get_main_option("sqlalchemy.url")
-    
+    """Run migrations in online mode."""
+
+    configuration = config.get_section(
+        config.config_ini_section
+    )
+
     connectable = engine_from_config(
         configuration,
         prefix="sqlalchemy.",
@@ -50,9 +53,10 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
+
         context.configure(
             connection=connection,
-            target_metadata=target_metadata
+            target_metadata=target_metadata,
         )
 
         with context.begin_transaction():
