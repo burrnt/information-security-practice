824e2080db1d2bf4a55b999cc9529f5636301496
burrnt <pogoriliy4@gmail.com>
2026-06-07 21:28:02 +0300

    Update app version to 0.5.0 and add routers
    Updated the application version to 0.5.0 and included additional routers for students, teachers, and admin.

--- app/main.py
+++ app/main.py
@@ -4,18 +4,26 @@ from app.database import engine, Base
 from app.seed import seed
 import app.models
 
+from app.routers import auth
+from app.routers import students
+from app.routers import teachers
+from app.routers import admin
+
 # Цей рядок автоматично створить .db файл та всі таблиці, якщо їх немає
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="Електронний деканат",
     description="API для управління академічними даними",
-    version="0.4.0"
+    version="0.5.0"
 )
 
 # Підключення роутерів
 app.include_router(auth.router)
- 
+app.include_router(students.router)
+app.include_router(teachers.router)
+app.include_router(admin.router)
+
 @app.get("/")
 def root():
 	return {"message":"Електронний деканат API v0.4.0"}
